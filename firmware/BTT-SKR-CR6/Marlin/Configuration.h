8edf5a3ddf9301aa13cf61174c0e1d9a8280d24a
unknown <1032616641@qq.com>
2021-04-12 10:51:22 +0800

    bugfix-4

--- firmware/BTT-SKR-CR6/Marlin/Configuration.h
+++ firmware/BTT-SKR-CR6/Marlin/Configuration.h
@@ -2184,7 +2184,7 @@
 // #define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRB  // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
-  #define NEOPIXEL_PIN    PB2      // LED driving pin
+  //#define NEOPIXEL_PIN    PB2      // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
   #define NEOPIXEL_PIXELS 4        // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
